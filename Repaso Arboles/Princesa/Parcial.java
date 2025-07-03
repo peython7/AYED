9d42c1f577aba451b72eb76604b629dc3292ef2c
Pedro Vega <103451058+peython7@users.noreply.github.com>
2025-07-03 17:29:07 -0300

    Create Parcial.java

---
+++ Repaso Arboles/Princesa/Parcial.java
@@ -0,0 +1,50 @@
+package ParcialPrincesa;
+
+import java.util.LinkedList;
+
+public class Parcial {
+
+	private GeneralTree<Personaje> arbol;
+	
+	public Parcial(GeneralTree<Personaje> a) {
+		this.arbol = a;
+	}
+	
+	public List<Personaje> devolverCamino(){
+		List<Personaje> lista = new LinkedList<Personaje>();
+		if(arbol != null && !arbol.isEmpty()) {
+			boolean ok = buscarPrincesa(lista,arbol);
+			return lista;
+		}
+		return null;
+	}
+	
+	private boolean buscarPrincesa(LinkedList<Personaje> l,GeneralTree<Personaje> a){
+		String personaje = a.getData(); 
+		if(personaje.getTipo() == "Dragon") {
+			return false;
+		}
+		if(personaje.getTipo() == "Princesa") {
+			l.add(personaje);
+			return true;
+		}
+		if(a.hasChildren()) {
+			boolean ok= true;
+			List<GeneralTree<Personaje>> temp = new LinkedList<GeneralTree<Personaje>>();
+			List<GeneralTree<Personaje>> children = a.getChildren();
+			for(GeneralTree<Personaje> c: children) {
+				ok= buscarPrincesa(l,c); 
+				if(ok) {
+					temp.add(a.getData());
+					temp.addAll(l);
+					l = temp;
+					return ok;
+				}else {
+					l.removeAll(l);
+					return ok;
+				}
+			}		
+		}
+		return false;
+	}
+}
